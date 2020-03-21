7bae67cc184a7f595e20bac236f05bc620018139
Max Wolschlager <comms@meks.io>
2020-03-21 18:45:38 +0100

    midi event handler;

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,18 @@
 #include <map>
 #include <vector>
 #include <cstdlib>
-#include <rtmidi/RtMidi.h>
-#include "lang.hpp"
-#include "rtseq.hpp"
-
 #include <thread>
 #include <atomic>
 #include <chrono>
 
+#include <rtmidi/RtMidi.h>
+#include "lang.hpp"
+#include "rtseq.hpp"
 
 
 //SECTION global defines and constants -----------
 
-#define INTERNAL_PPQN 960
+#define INTERNAL_PPQN MIDI_PULSES_PQN
 #define DEFAULT_EXTERNAL_PPQN 24
 
 
@@ -33,7 +32,7 @@ struct global_settings {
   bool FOLLOW_INPUT_CLOCK;
   uint_fast16_t INPUT_PPQN;
   bool FOLLOW_INPUT_STARTSTOP;
-}GLOBAL_SETTINGS{false,DEFAULT_EXTERNAL_PPQN,false};
+} GLOBAL_SETTINGS { false, DEFAULT_EXTERNAL_PPQN, false };
 
 class globalAtomics {
   atomic<bool> running;
@@ -62,14 +61,49 @@ extern map<int, SequenceNode *> eventMap;
 
 
 //SECTION function definitions --------------------
+auto now = chrono::steady_clock::now();
+auto lastPulse = now;
+auto delta = now - lastPulse;
 
-void onmessage( double deltatime, std::vector< unsigned char > *message, void *userData )
+void handleClockPulse(vector<unsigned char> *message) {
+  printf("Received Midi Clock Pulse Message \n");
+  now = chrono::steady_clock::now();
+  delta = now - lastPulse;
+  int bpm = (((1000. * 1000 * 1000 * 60) / delta.count())) / GLOBAL_SETTINGS.INPUT_PPQN;
+  printf("Estimated BPM: %d\n", bpm);
+
+  lastPulse = now;
+}
+
+void onmessage( double deltatime, vector<unsigned char> *message, void *userData )
 {
-  unsigned int nBytes = message->size();
-  for ( unsigned int i=0; i<nBytes; i++ )
-    std::cout << "Byte " << i << " = " << (int)message->at(i) << ", ";
-  if ( nBytes > 0 )
-    std::cout << "stamp = " << deltatime << std::endl;
+  unsigned char status = message->at(0);
+  switch(status) {
+    case MIDI_TIME_CLOCK_BYTE:
+      handleClockPulse(message);
+      break;
+    case MIDI_START_BYTE: 
+      printf("Received Midi Clock Start Message \n");
+      break;
+    case MIDI_CONTINUE_BYTE: 
+      printf("Received Midi Clock Continue Message \n");
+      break;
+    case MIDI_STOP_BYTE: 
+      printf("Received Midi Clock Stop Message \n");
+      break;
+    case MIDI_NOTE_ON_BYTE: 
+      printf("Received Midi Note On Message \n");
+      break;
+    case MIDI_NOTE_OFF_BYTE: 
+      printf("Received Midi Note Off Message \n");
+      break;
+    case MIDI_POLY_AFTER_BYTE: 
+      printf("Received Midi Note Aftertouch Message \n");
+      break;
+    default:
+      fprintf(stderr, "Unknown Midi Message: 0x%x \n", status);
+      break;
+  }
 }
 
 
@@ -90,8 +124,9 @@ int main(int argc, char* argv[]) {
 
   yyparse();
 
-  RtEvent *start = eventMap[-2]->renderRtEvents(1,1);
-  start->run();
+  RtEvent *start = eventMap[-2]->renderRtEvents(1, 1);
+  Context rtContext({});
+  start->run(rtContext); 
 
   RtMidiIn *midiin = new RtMidiIn();
   RtMidiOut *midiout = new RtMidiOut();
