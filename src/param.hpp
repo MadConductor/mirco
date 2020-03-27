737669b299f93a8e64598de0a55b059b72996ebf
Lukas Hannen <git.c@hannen.at>
2020-03-27 23:14:01 +0100

    Beautyfying the param.hpp

--- src/param.hpp
+++ src/param.hpp
@@ -30,21 +30,22 @@ public:
 
 using puft = param<uint_fast16_t>;
 using pbol = param<bool>;
-using papi = param<RtMidi::Api>;
+using rtma = RtMidi::Api;
+using papi = param<rtma>;
 
 struct global_settings {
 public:
-  puft INPUT_PORT             = puft(0,                     {"input",      required_argument, 0, 'i'});
-  puft OUTPUT_PORT            = puft(0,                     {"output",     required_argument, 0, 'o'});
-  pbol FOLLOW_INPUT_CLOCK     = pbol(false,                 {"follow-clk", optional_argument, 0, 'c'});
-  puft INPUT_PPQN             = puft(24,                    {"",           no_argument,       0, '?'});
+  puft INPUT_PORT         = puft(0,                 {"input",      required_argument, 0, 'i'});
+  puft OUTPUT_PORT        = puft(0,                 {"output",     required_argument, 0, 'o'});
+  pbol FOLLOW_INPUT_CLOCK = pbol(false,             {"follow-clk", optional_argument, 0, 'c'});
+  puft INPUT_PPQN         = puft(24,                {"",           no_argument,       0, '?'});
 
-  puft DEFAULT_BPM            = puft(120,                   {"bpm",        required_argument, 0, 'b'});
+  puft DEFAULT_BPM        = puft(120,               {"bpm",        required_argument, 0, 'b'});
 
-  papi BACKEND                = papi(RtMidi::Api::UNSPECIFIED, {"api",     required_argument, 0, 'a'});
+  papi BACKEND            = papi(rtma::UNSPECIFIED, {"api",        required_argument, 0, 'a'});
 
 
-  pbol FOLLOW_INPUT_STARTSTOP = pbol(false,                 {"",           no_argument,       0, '?'});
+  pbol INPUT_CONTROLS     = pbol(false,             {"",           no_argument,       0, '?'});
 
   const struct option LONG_OPTS[6] = {
                                                 INPUT_PORT.OPT,
